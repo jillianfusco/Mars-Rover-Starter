bce195243ba9b42a4442dec9abaf05592ef2a93d
Jillian Fusco <fuscojillian@gmail.com>
2024-07-22 09:00:46 -0400

    task 2 complete

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,8 +5,23 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 
+// test 1
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+    let errorMessage = 'Command type required.'
+    let instantiationAttempt = function() { new Command();}
+    expect(instantiationAttempt).toThrow(new Error(errorMessage));
+  });
+
+  // test 2
+  it("constructor sets command type", function() {
+    let modeCommand = new Command("MODE_CHANGE");
+    expect(modeCommand).toBeInstanceOf(Command);
+  });
+
+  // test 3
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    let moveCommand = new Command("MOVE", 12000);
+    expect(moveCommand.value).toEqual(12000);
   });
 
 });
